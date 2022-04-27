e8ad81d24c65c2beb82ffddf32607a5c87fcc56c
dalexez <dalexez@gmail.com>
2022-04-27 14:09:27 -0500

    Agregando otras validaciones en la prueba de aplyValidationInExplorer

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -5,11 +5,32 @@ describe("Unit Tests for ExplorerService Class", () => {
         const explorer1 = {name: "Explorer1", score: 1}
         const explorerFizzbuzz1 = FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1}
 
+        expect(explorerFizzbuzz1.trick).toBe(explorerFizzbuzz1.score)
+    })
+    test("2)Test aplyValidationInExplorer trick = FIZZ", () => {
         const explorer3 = {name: "Explorer3", score: 3}
         const explorerFizzbuzz3 = FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-       
-        expect(explorerFizzbuzz1.trick).toBe(explorerFizzbuzz1.score)
-        expect(explorerFizzbuzz3.trick).toBe("FIZZ");
-      
+
+        expect(explorerFizzbuzz3.trick).toBe("FIZZ")
+    })
+    test("3)Test aplyValidationInExplorer trick = BUZZ", () => {
+        const explorer5 = {name: "Explorer5", score: 5}
+        const explorerFizzbuzz5 = FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+        expect(explorerFizzbuzz5.trick).toBe("BUZZ")
+    })
+    test("4)Test aplyValidationInExplorer trick = FIZZBUZZ", () => {
+        const explorer15 = {name: "Explorer15", score: 15}
+        const explorerFizzbuzz15 = FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+
+        expect(explorerFizzbuzz15.trick).toBe("FIZZBUZZ")
     })
 })
+
+
+        
+       
+        
+
+        
+      
