75ef5662d6f6ad6051ad5f748dddf30bc39f2e7c
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-26 23:52:23 -0500

    Agregando metodo filterByMission de la clase ExplorerService y su prueba PASS

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,7 +3,7 @@ const ExplorerService = require('./../../lib/services/ExplorerService')
 describe("Unit Tests for ExplorerService Class", () => {
     test('1) Test filterByMision', () => {
       
-        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}]
+        const explorers = [{mission: "node"}, {mission: "java"}, {mission: "node"}]
         const explorersInNode  = ExplorerService.filterByMission(explorers, "node")
        
         expect(explorersInNode.length).toBe(2);
