cb1ee25fcfd1f9544029c5fae0475ab3894bd706
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-27 00:05:26 -0500

    Agregando prueba para metodo amountOfExplorersByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -8,5 +8,13 @@ describe("Unit Tests for ExplorerService Class", () => {
        
         expect(explorersInNode.length).toBe(2);
       
+    })
+    test('2) Test getAmountOfExplorersByMission', () => {
+      
+        const explorers = [{mission: "node"}, {mission: "java"}, {mission: "node"}]
+        const amountOfExplorersInMission  = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+       
+        expect(amountOfExplorersInMission.length).toBe(2);
+      
     });
   })
