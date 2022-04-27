7d54cf9629f017a5ec2d7505c76d23bbbb13d6b1
dalexez <dalexez@gmail.com>
2022-04-27 15:18:03 -0500

    Mejorando formato del codigo

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,28 +1,23 @@
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const ExplorerService = require('../../lib/services/ExplorerService');
 
-describe("Unit Tests for ExplorerService Class", () => {
-    test('1) Test filterByMision', () => {
-      
-        const explorers = [{mission: "node"}, {mission: "java"}, {mission: "node"}]
-        const explorersInNode  = ExplorerService.filterByMission(explorers, "node")
-       
-        expect(explorersInNode.length).toBe(2);
-      
-    })
-    test('2) Test getAmountOfExplorersByMission', () => {
-      
-        const explorers = [{mission: "node"}, {mission: "java"}, {mission: "node"}]
-        const amountOfExplorersInMission  = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
-       
-        expect(amountOfExplorersInMission).toBe(2);
-      
-    })
-    test('3) Test getExplorersUsernamesByMission', () => {
-      
-        const explorers = [{githubUsername: "dalexez", mission: "node"}, {githubUsername: "danex", mission: "java"}, {githubUsername:"dalex", mission: "node"}]
-        const explorersUsernameInMission  = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
-       
-        expect(explorersUsernameInMission).toStrictEqual(["dalexez", "dalex"])//Con toBe la prueba fallaba ya que por alguna razon toma diferentes los arreglos
-        expect(explorersUsernameInMission.length).toBe(2)
-    })
-  })
+describe('Unit Tests for ExplorerService Class', () => {
+  test('1) Test filterByMision', () => {
+    const explorers = [{ mission: 'node' }, { mission: 'java' }, { mission: 'node' }];
+    const explorersInNode = ExplorerService.filterByMission(explorers, 'node');
+
+    expect(explorersInNode.length).toBe(2);
+  });
+  test('2) Test getAmountOfExplorersByMission', () => {
+    const explorers = [{ mission: 'node' }, { mission: 'java' }, { mission: 'node' }];
+    const amountOfExplorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, 'node');
+
+    expect(amountOfExplorersInMission).toBe(2);
+  });
+  test('3) Test getExplorersUsernamesByMission', () => {
+    const explorers = [{ githubUsername: 'dalexez', mission: 'node' }, { githubUsername: 'danex', mission: 'java' }, { githubUsername: 'dalex', mission: 'node' }];
+    const explorersUsernameInMission = ExplorerService.getExplorersUsernamesByMission(explorers, 'node');
+
+    expect(explorersUsernameInMission).toStrictEqual(['dalexez', 'dalex']);// Con toBe la prueba fallaba ya que por alguna razon toma diferentes los arreglos
+    expect(explorersUsernameInMission.length).toBe(2);
+  });
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,11 +1,10 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require('../../lib/utils/Reader');
 
-describe("Unit Tests for Reader Class", () => {
-    test('1) Read the path of a file and the data on it', () => {
-      
-      const explorers  = Reader.readJsonFile("explorers.json")
-       
-      expect(explorers.length).toBe(15);
-      expect(explorers[1].name).toBe("Woopa2")
-    });
-  })
+describe('Unit Tests for Reader Class', () => {
+  test('1) Read the path of a file and the data on it', () => {
+    const explorers = Reader.readJsonFile('explorers.json');
+
+    expect(explorers.length).toBe(15);
+    expect(explorers[1].name).toBe('Woopa2');
+  });
+});
