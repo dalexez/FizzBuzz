72d34ea52589b4787f1f43184a7ff9d030d5799a
dalexez <dalexez@gmail.com>
2022-04-28 13:10:31 -0500

    Corrigiendo formato de codigo con lint

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,19 +1,19 @@
-const ExplorerController = require('./../../lib/controllers/ExplorerController')
+const ExplorerController = require('../../lib/controllers/ExplorerController');
 
 describe('Unit Tests for ExplorerController Class', () => {
-    test('1) Test for getExplorerByMission', () => {
-        const explorersByMission = ExplorerController.getExplorersByMission("java")
+  test('1) Test for getExplorerByMission', () => {
+    const explorersByMission = ExplorerController.getExplorersByMission('java');
 
-        expect(explorersByMission.length).toBe(5)
-    })
-    test('2) Test for getExplorersUsernamesByMission on class ExplorerController', () => {
-        const usernamesByMission = ExplorerController.getExplorersUsernamesByMission("node")
+    expect(explorersByMission.length).toBe(5);
+  });
+  test('2) Test for getExplorersUsernamesByMission on class ExplorerController', () => {
+    const usernamesByMission = ExplorerController.getExplorersUsernamesByMission('node');
 
-        expect(usernamesByMission.length).toBe(10)
-    })
-    test('3) Test for getExplorersAmountByMission on class ExplorerController', () => {
-        const amountByMission = ExplorerController.getExplorersAmountByMission("java")
+    expect(usernamesByMission.length).toBe(10);
+  });
+  test('3) Test for getExplorersAmountByMission on class ExplorerController', () => {
+    const amountByMission = ExplorerController.getExplorersAmountByMission('java');
 
-        expect(amountByMission).toBe(5)
-    })
-})
+    expect(amountByMission).toBe(5);
+  });
+});
