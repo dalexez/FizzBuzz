5a21628e965ef3eb6a5d9c313aa33ce185c4c935
dalexez <dalexez@gmail.com>
2022-04-28 11:39:00 -0500

    Agregando prueba unitaria para metodo getExplorersAmountByMission en la clase ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -6,9 +6,14 @@ describe('Unit Tests for ExplorerController Class', () => {
 
         expect(explorersByMission.length).toBe(5)
     })
-    test('2)Test for getExplorersUsernamesByMission on class ExplorerController', () => {
+    test('2) Test for getExplorersUsernamesByMission on class ExplorerController', () => {
         const usernamesByMission = ExplorerController.getExplorersUsernamesByMission("node")
 
         expect(usernamesByMission.length).toBe(10)
+    })
+    test('3) Test for getExplorersAmountByMission on class ExplorerController', () => {
+        const amountByMission = ExplorerController.getExplorersAmountByMission("java")
+
+        expect(amountByMission).toBe(5)
     })
 })
